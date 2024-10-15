abcf773e4aca9b46ebf49dbf4ef4eb8123ee821f
Oliver-Edwards290 <121872973+Oliver-Edwards290@users.noreply.github.com>
2024-10-15 14:31:40 +0100

    Lightning and lights
    also added good and bad end screens

--- Art Assets/ArtScripts/LightningFlash.gd
+++ Art Assets/ArtScripts/LightningFlash.gd
@@ -1,11 +1,15 @@
-extends Sprite2D
+extends Node2D
+
+@export var TimerLength: float =  0
+
+func _ready() -> void:
+	$Sprite2D.hide()
+	randomize()
+	$Sprite2D/Timer.wait_time = randi_range(1,10)
 
 
-
-
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	self.visible = true
+func _on_timer_timeout() -> void:
+	$Sprite2D.show()
+	await get_tree().create_timer(0.1,0.3).timeout
+	$Sprite2D.hide()
+		
