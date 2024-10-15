43281daf9ae15905e3a7389c4ecf6370abb1ed16
LesterCantor <c012718j@student.staffs.ac.uk>
2024-10-15 18:58:57 +0100

    flip sprite depending on direction

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -7,6 +7,7 @@ class_name Enemy
 
 @onready var detection_range: Area2D = $DetectionRange
 @onready var flip_direction: Node2D = $FlipDirection
+@onready var animated_sprite_2d: AnimatedSprite2D = $FlipDirection/AnimatedSprite2D
 
 var move_direction: Vector2 = Vector2.RIGHT:
 	set(new_direction):
@@ -19,8 +20,12 @@ func _physics_process(delta: float) -> void:
 	if player != null:
 		var direction: Vector2 = global_position.direction_to(player.global_position)
 		velocity = velocity.move_toward(direction.normalized() * SPEED, ACCELERATION * delta)
+		if direction.x < 0:
+			move_direction = Vector2.LEFT
+		else:
+			move_direction = Vector2.RIGHT
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	print(velocity.y)
+
 	move_and_slide()
