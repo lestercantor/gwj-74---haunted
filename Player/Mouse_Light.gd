957ce54450e2450431fef8e3adf5f915892d1b09
LesterCantor <c012718j@student.staffs.ac.uk>
2024-10-15 18:47:10 +0100

    activate light when holding down left click
    tried to make the light colour to be more of a flame but doesn't look right to me... can you fix?

--- Player/Mouse_Light.gd
+++ Player/Mouse_Light.gd
@@ -3,5 +3,15 @@ class_name MouseLight
 
 @export var light: PointLight2D
 
+func _ready() -> void:
+	light.texture_scale = 7
+	light.enabled = false
+	
 func _process(delta: float) -> void:
 	light.global_position = get_global_mouse_position()
+	if Input.is_action_just_pressed("activate_light"):
+		light.enabled = true
+		
+		
+	elif Input.is_action_just_released("activate_light"):
+		light.enabled = false
