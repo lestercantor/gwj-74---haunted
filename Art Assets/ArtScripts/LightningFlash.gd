52aab038f47ed519452f6117443c5c086625c490
Oliver-Edwards290 <121872973+Oliver-Edwards290@users.noreply.github.com>
2024-10-17 00:29:07 +0100

    main menu tweaks
    settings button (no functionality)
    added light to the moon

--- Art Assets/ArtScripts/LightningFlash.gd
+++ Art Assets/ArtScripts/LightningFlash.gd
@@ -10,6 +10,6 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	$Sprite2D.show()
-	await get_tree().create_timer(0.1,0.3).timeout
+	await get_tree().create_timer(0.2).timeout
 	$Sprite2D.hide()
 		
