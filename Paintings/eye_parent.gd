22d0011b4629c48c5e1110f48807ffcfd8cbbf2d
Oliver-Edwards290 <121872973+Oliver-Edwards290@users.noreply.github.com>
2024-10-20 18:31:32 +0100

    fixed eyes

--- Paintings/eye_parent.gd
+++ Paintings/eye_parent.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GlobalSignals.painting_burned.connect(disable_collision)
+	pass#GlobalSignals.painting_burned.connect(disable_collision)
 
 func disable_collision() -> void:
 	collision_shape_2d.disabled = true
