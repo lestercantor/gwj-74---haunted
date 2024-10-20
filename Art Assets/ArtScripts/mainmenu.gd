9619fbabc084738b8d2ddc63d344a81276e63116
Oliver-Edwards290 <121872973+Oliver-Edwards290@users.noreply.github.com>
2024-10-20 13:50:05 +0100

    settings menu
    also fixed stair collision (the player was slightly incorrectly scaled)

--- Art Assets/ArtScripts/mainmenu.gd
+++ Art Assets/ArtScripts/mainmenu.gd
@@ -13,4 +13,4 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Art Assets/Art Scenes/GoodEndScene.tscn")
+	get_tree().change_scene_to_file("res://Art Assets/Art Scenes/SettingsMenu.tscn")
--- Pause Menu/pause_menu.gd
+++ Pause Menu/pause_menu.gd
@@ -6,3 +6,13 @@ func _notification(what: int) -> void:
 			hide()
 		Node.NOTIFICATION_UNPAUSED:
 			show()
+
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Art Assets/Art Scenes/MainMenuBackground.tscn")
+
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
