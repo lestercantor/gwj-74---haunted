51597958c091a8db6e1cb3b973c631ecf3004c36
LesterCantor <c012718j@student.staffs.ac.uk>
2024-10-20 19:01:03 +0100

    slight player nerf to speed when near eyes. set cursor to default after pausing

--- Level/GameplayManager.gd
+++ Level/GameplayManager.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		toggle_pause()
+		Input.set_custom_mouse_cursor(null)
 		
 func toggle_pause() -> void:
 	get_tree().paused = !get_tree().paused
